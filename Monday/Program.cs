8678625d761b9049331d2d5749610115758f85ca
Opilane TTHK <opilane@tthk.ee>
2020-11-16 15:53:13 +0200

    Monday

---
+++ Monday/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Monday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            GetDirectories(rootPath); 
+
+        }
+        public static void GetDirectories(string path)
+        {
+            string[] allDirectories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            string filePath = @"C:\Users\opilane\Monday\DirectoriesData.txt";
+            File.WriteAllLines(filePath, allDirectories);
+        }
+        public static void GetFiles(string path)
+        {
+            string[] allFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            string filePath = @"C:\Users\opilane\Monday\FilesData.txt";
+            File.WriteAllLines(filePath, allFiles);
+
+        }
+    }
+
+
+
+
+    
+
+    
+}
